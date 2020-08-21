446f02afb714703fb17cc5471f7cc5c40d3787fc
Barrettb2 <70039690+Barrettb2@users.noreply.github.com>
2020-08-21 18:53:48 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/**************************************
+*Java Program Name
+***************************************
+*Student Name (first and last)
+*Date
+*Course Number and Section
+**************************************/
+
+public class Hello{
+
+	public static void main (String[] args){
+	System.out.println("<Brianna Barrett>");
+	System.out.println("<Hello.java>");
+	System.out.println("<Lab 01>");
+	System.out.println("<8/21/2020>");
+	System.out.println("<CMSC 255-C90>");
+	
+
+	}
+	
+}
